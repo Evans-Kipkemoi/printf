a1b90d69d7caa4cf08725f4964bd4d2c57807ad5
Evans-Kipkemoi <evanskipkemoi7@gmail.com>
2022-07-12 17:03:24 -0700

    Edit on main.h file, adding print address prototype

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <unistd.h>
 #include <stdlib.h>
@@ -19,8 +19,6 @@ typedef struct convert conver_t;
 
 /*Main functions*/
 int parser(const char *format, conver_t f_list[], va_list arg_list);
-int _printf(const char *format, ...);
-int _write_char(char);
 int print_char(va_list);
 int print_string(va_list);
 int print_percent(va_list);
@@ -34,14 +32,36 @@ int print_octal(va_list list);
 int print_hex(va_list list);
 int print_heX(va_list list);
 
+/* _printf */
+int _printf(const char *format, ...);
+
+/* write_char */
+int _write_char(char c);
+int _putchar(char c);
+
+/* print_number */
+int print_number(va_list args);
+int print_unsgined_number(unsigned int n);
+
+
+
 /* get_flag */
 int get_flag(char s, flags_t *f);
 
 /* print_address */
 int print_address(va_list l, flags_t *f);
 
+/* print_custom */
+int print_bigS(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_rot13(va_list l, flags_t *f);
+
+/* print_percent */
+int print_percent(va_list l, flags_t *f);
+
+
 /*Helper functions*/
-unsigned int base_len(unsigned int, int);
+iunsigned int base_len(unsigned int, int);
 char *rev_string(char *);
 void write_base(char *str);
 char *_memcpy(char *dest, char *src, unsigned int n);
